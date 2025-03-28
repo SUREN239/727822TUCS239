5a5f7203ba3b889e986db54970b5743d7cc87d28
SUREN S <surenmanju2004@gmail.com>
2025-03-28 13:37:20 +0530

    Question 1 - Project Created
    Social Media Analytics

---
+++ Social-Media-Analytics/src/test/java/com/test/socialmediaanalytics/SocialMediaAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.socialmediaanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
